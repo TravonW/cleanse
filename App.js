6055a7405ba6958a09921def1e4826d27c028cbc
TXT-57 <travonwilliams321@gmail.com>
2023-07-14 20:35:22 -0700

    icons

--- App.js
+++ App.js
@@ -1,52 +1,70 @@
-import { Text, View, Image, Button } from "react-native";
+import { Text, View, Image, Button, StyleSheet } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import styles from "./assets/styles/styles";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Tracking from "./Tabs/tracking";
-import Camera from "./Tabs/camera";
-import Recycle from "./Tabs/Recycle";
-
-
+import Camera from "./Tabs/Camera";
+import Recycle from "./Tabs/Donate";
 import { NavigationContainer } from "@react-navigation/native"
+import Tracking from "./Tabs/Tracking";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 
 
-const Tab = createBottomTabNavigator();
-
-
-
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Tracking" component={Tracking} />
-        <Tab.Screen name="Camera" component={Camera} />
-        <Tab.Screen name="Recycle" component={Recycle} />
+
+    <NavigationContainer >
+      <Tab.Navigator >
+        {/* To adjust styles of components use this line */}
+        <Tab.Screen name="Garment Tracking" style={styles.navbar} component={Tracking} options={{
+          headerTitleAlign: 'center', tabBarIcon: () => (
+            <MaterialCommunityIcons name="tshirt-crew" color={"red"} size={26} />
+          ),
+        }} />
+        <Tab.Screen name="Camera" component={Camera} options={{
+          headerTitleAlign: 'center', tabBarIcon: () => (<MaterialCommunityIcons name="camera" color={"red"} size={26} />
+          ),
+        }} />
+        <Tab.Screen name="Donate" component={Recycle}  options={{
+          headerTitleAlign: 'center', tabBarIcon: () => (<FontAwesome5 name="hand-holding-heart" color={"red"} size={26} />
+          ),
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
-
-
-    // <View style={styles.container}>
-    //   <Text style={styles.headerName}> Garmant Tracking </Text>
-
-
-    //   {/* Make search bar, make it a form */}
-
-    //  {/* Make filter button */}
-
-    //  <View style={styles.filterContainer}>
-    //      <Button title="Filter" style={styles.filter} color="black"   onPress={filterOptions}></Button>
-    //     <Ionicons name="filter" size={24} color= "black" position />
-    //     </View>
-
-    //     {/* Make bottom navigation bar */}
-    //     <View>
-
-    //     </View>
-
-
-
-    //   </View>
   );
 }
+
+
+// <View style={styles.container}>
+//   <Text style={styles.headerName}> Garmant Tracking </Text>
+
+
+//   {/* Make search bar, make it a form */}
+
+//  {/* Make filter button */}
+
+//  <View style={styles.filterContainer}>
+//      <Button title="Filter" style={styles.filter} color="black"   onPress={filterOptions}></Button>
+//     <Ionicons name="filter" size={24} color= "black" position />
+//     </View>
+
+//     {/* Make bottom navigation bar */}
+//     <View>
+
+//     </View>
+
+
+
+//   </View>
+
+
+const styles = StyleSheet.create({
+  navbar: {
+    flex: 1,
+    justifyContent: "center",
+    color: 'red !important',
+  }
+});
--- Tabs/Tracking.js
+++ Tabs/Tracking.js
@@ -1,20 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native-paper";
+
+
+/// functions //
+const filterOptions = () =>{
+    console.log('hello')
+}
 
 export default function Tracking() {
     return (
-        <View style={styles.container}>
-            <Text> Hello from Tracking</Text>
+        <SafeAreaView style={styles.container}>
+            <TextInput  style ={ styles.searchBar}> hi </TextInput>
+            <Pressable onPress={filterOptions} >
+                <Text style={styles.filterText}>Filter</Text>
+                {/* <Ionicons size={30} style={styles.filterImage} /> */}
+                <Ionicons name="filter" size={24} style={styles.filterImage}/>
+            </Pressable>
             {/* <Image source={} /> */}
             {/* <StatusBar style="auto" /> */}
-        </View>
+        </SafeAreaView>
     );
 }
 
+
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+        position: "relative",
+
+        backgroundColor: 'green',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    filterText: {
+        position: "absolute",
+        top: 125,
+        right: 40,
+        color: "red",
+        fontSize: 35,
+    },
+    filterImage: {
+        position: "absolute",
+        top: 135,
+        right: 10,
+        color: "red",
+    },
+    searchBar:{
+        position: "absolute",
+        top: 120,
+        right: 30,
+    },
+    
 });
